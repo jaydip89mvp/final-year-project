1acad00b6acab6dcd00fe9fbe2cf2469f1682668
ashutoshk-01 <ashukirjat@gmail.com>
2026-01-23 20:49:00 +0530

    Initial project structure setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className=''>
+      Adaptive Learning Platform
+     </div>
+    </>
+  )
+}
+
+export default App
+
+
